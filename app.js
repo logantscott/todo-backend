b0f1d10794b9b42cf1d0bbb22bfb03b40969cdba
Logan Scott <logantscott@gmail.com>
2020-02-25 18:13:34 -0800

    endpoints set up

--- app.js
+++ app.js
@@ -37,6 +37,66 @@ app.get('/', async(req, res, next) => {
     }
 });
 
+app.get('/todos', async(req, res, next) => {
+    try {
+        const result = await client.query(`
+            SELECT
+                *
+            FROM todos;
+        `);
+
+        res.json(result.rows);
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.post('/todos', async(req, res, next) => {
+    try {
+        const result = await client.query(`
+        INSERT INTO todos (task, complete)
+        VALUES ($1, false)
+        RETURNING *;
+        `,
+        [req.body.task]);
+
+        res.json(result.rows[0]);
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.put('/todos', async(req, res, next) => {
+    try {
+        const result = await client.query(`
+        UPDATE todos
+        SET complete = $1
+        WHERE todos.id = $2
+        RETURNING *;
+        `,
+        [req.body.complete, req.body.id]);
+
+        res.json(result.rows[0]);
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.delete('/todos', async(req, res, next) => {
+    try {
+        const result = await client.query(`
+        DELETE FROM todos
+        WHERE todos.id = $1
+        RETURNING *;
+        `,
+        [req.body.id]);
+
+        res.json(result.rows[0]);
+    } catch (err) {
+        next(err);
+    }
+});
+
 // http method and path...
 
 module.exports = {
